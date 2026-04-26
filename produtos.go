a520a903c4de3559d30f3f0aead290d27d825664
João César Rodrigues Ferreira <66702938+JoaoCesarDev@users.noreply.github.com>
2026-04-26 18:42:15 -0300

    exercício de um sistema simples de estudo em Golang

---
+++ produtos.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Produto struct {
+	nome  string
+	preco float64
+}
+
+func main() {
+	produto1 := Produto{"Produto 1", 10.0}
+	produto2 := Produto{"Produto 2", 20.0}
+	produto3 := Produto{"Produto 3", 30.0}
+
+	produtos := []Produto{produto1, produto2, produto3}
+
+	maiorPreco := produtos[0]
+
+	for _, produto := range produtos {
+		if produto.preco > maiorPreco.preco {
+			maiorPreco = produto
+		}
+	}
+
+		fmt.Printf(" O produto de Maior Preço: %s, %.2f\n", maiorPreco.nome, maiorPreco.preco)
+
+}
